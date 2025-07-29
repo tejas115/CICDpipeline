b772d5d18c577f9e21fde6a143a43ceb9b08bee5
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-07-29 11:17:49 -0500

    Final CI/CD pipeline test

--- todo-api/main.py
+++ todo-api/main.py
@@ -10,8 +10,8 @@ hits = Counter("hits", "Number of hits to the root")
 def read_root():
     hits.inc()
     # return {"message": "Hello World"}
-    return {"message": f"Hello from CI/CD Pipeline! 🚀 Hits: {hits._value.get()}"} #changed API response from default. 
-
+    # return {"message": f"Hello from CI/CD Pipeline! 🚀 Hits: {hits._value.get()}"} #changed API response from default. 
+    return {"message": f"🚀 FINAL TEST - CI/CD COMPLETE! Hits: {hits._value.get()}"}
 
 @app.get("/metrics")
 def metrics():
